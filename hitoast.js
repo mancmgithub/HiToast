839d31d0a43732ab0e7f875e3bb11f11c3faf044
hicoldcat <1351290389@qq.com>
2016-08-10 18:08:39 +0800

    create hitoast.js

---
+++ hitoast.js
@@ -0,0 +1,32 @@
+/**
+ * HiToast.js
+ * Notification Library similar to Toast For Mobile Apps
+ * @author HiColdCat
+ * @license MIT
+ * See more at: https://github.com/hicoldcat/HiToast
+ */
+
+/*
+*Params:
+*	text: text want to show
+*	options: object that about the style
+*		thems: Default 'Info' .  ('Success','Warning','Error','Info') can be chosen.
+*		icon: Default null .  Class of the icon to show before the alert.
+*		duration: Default 3000ms . Duration of the notification in ms.
+*		fade-time: Default 'fast' . Duration of the fade in/out of the notification.  'fast','slow' or time in ms can be chosen.
+*		position: Default 'buttom' . ('top','center','bottom') can be chosen.
+*/
+function hiToast(text,options) {
+	
+}
+
+/*Removes a toast from the page
+* Params:
+ *    Called without arguments, the function removes all alerts
+ *    element: a jQuery object to remove
+ *    options:
+ *    	duration: Default 'fast' .  Duration of the alert fade out .  'fast', 'slow' or time in ms can be chosen .
+ */
+function hiToastX(element,options) {
+	
+}
