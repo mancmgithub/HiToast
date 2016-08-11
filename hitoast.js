41cf48dc31f8c0229b17e605c923ce31faf62631
hicoldcat <1351290389@qq.com>
2016-08-11 10:56:06 +0800

    create index.html and modify hitoast.js method

--- hitoast.js
+++ hitoast.js
@@ -11,13 +11,39 @@
 *	text: text want to show
 *	options: object that about the style
 *		thems: Default 'Info' .  ('Success','Warning','Error','Info') can be chosen.
-*		icon: Default null .  Class of the icon to show before the alert.
 *		duration: Default 3000ms . Duration of the notification in ms.
-*		fade-time: Default 'fast' . Duration of the fade in/out of the notification.  'fast','slow' or time in ms can be chosen.
-*		position: Default 'buttom' . ('top','center','bottom') can be chosen.
+*		fade-time: Default 'normal' . Duration of the fade in/out of the notification.  'fast','slow' ,'normal'or time in ms can be chosen.
+*		position: Default 'bottom' . ('top','center','bottom') can be chosen.
+*		container-id: Default 'hitoast' . Id of the alert container. 
 */
 function hiToast(text,options) {
+	var defaultOptions = {
+		'thems'	: 'Info',		//'Info','Success','Waring','Error'
+		'duration'	: 3000,		//Duration of the notification in ms
+		'fade-time'	: 'normal',		//'fast','slow','normal'or integer in ms
+		'position'	: 'bottom',		//'top','center','bottom'
+		'container-id'	: 'hitoast'		// Id of the alert container. 
+	};
+
+	options = (typeof options == 'object') ? $.extend(defaultOptions, options) : defaultOptions;
+
+	var $container = $('#' + options['container-id']),
+	thems_markup = '';
+	if (options.thems) {
+		thems_markup = '-' + options.thems;
+	}
+
+	var html = $('<div class="toast' + thems_markup + '">' + text + '</div>').fadeIn(options['fade-time']);
 	
+	$container.append(html);
+
+	html.on('click', function() {
+		hiToastX($(this));
+	});
+
+	setTimeout(function () {
+		hiToastX(html);
+	},options.duration);
 }
 
 /*Removes a toast from the page
@@ -27,6 +53,20 @@ function hiToast(text,options) {
  *    options:
  *    	duration: Default 'fast' .  Duration of the alert fade out .  'fast', 'slow' or time in ms can be chosen .
  */
-function hiToastX(element,options) {
-	
-}
+ function hiToastX(element,options) {
+ 	defaultOptions = {
+ 		'duration'	: 'fast'
+ 	};
+
+ 	options = (typeof options == 'object') ? $.extend(defaultOptions, options) : defaultOptions;
+
+ 	if (typeof element !== "undefined") {
+ 		element.fadeOut(options.duration, function() {
+ 			$(this).remove();
+ 		});
+ 	} else {
+ 		$('.toast').fadeOut(options.duration, function() {
+ 			$(this).remove();
+ 		});
+ 	}
+ }
